f993da5ef2d16829236b9445078b86751f99f3ab
Johnathan Sattler <jsattler@outlook.com>
2016-11-21 14:26:52 -0500

    Added smaller cube

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function initShaders() {
 var mvMatrix = mat4.create();
 var pMatrix = mat4.create();
 
-function setMatrixUniformsSkybox() {
+function setMatrixUniforms() {
     gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
     gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
 }
@@ -235,6 +235,117 @@ function initBuffers() {
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
     skyboxVertexIndexBuffer.itemSize = 1;
     skyboxVertexIndexBuffer.numItems = 36;
+
+    initCube();
+}
+
+var cubeVertexPositionBuffer;
+var cubeVertexTextureCoordBuffer
+var cubeVertexNormalBuffer;
+var cubeVertexIndexBuffer;
+
+function initCube() {
+
+
+    cubeVertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
+    var vertices = [
+        // Front face
+        -1.0, -1.0, 1.0,
+         1.0, -1.0, 1.0, 
+         1.0,  1.0, 1.0,
+        -1.0,  1.0, 1.0,
+
+         // Back face
+        -1.0, -1.0, -1.0,
+        -1.0,  1.0, -1.0,
+         1.0,  1.0, -1.0,
+         1.0, -1.0, -1.0,
+
+         // Top face
+        -1.0, 1.0, -1.0,
+        -1.0, 1.0,  1.0,
+         1.0, 1.0,  1.0,
+         1.0, 1.0, -1.0,
+
+         // Bottom face
+        -1.0, -1.0, -1.0,
+         1.0, -1.0, -1.0,
+         1.0, -1.0,  1.0,
+        -1.0, -1.0,  1.0,
+
+        // Right face
+        1.0, -1.0, -1.0,
+        1.0,  1.0, -1.0,
+        1.0,  1.0,  1.0,
+        1.0, -1.0,  1.0,
+
+        // Left fae
+        -1.0, -1.0, -1.0,
+        -1.0, -1.0,  1.0,
+        -1.0,  1.0,  1.0,
+        -1.0,  1.0, -1.0
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    cubeVertexPositionBuffer.itemSize = 3;
+    cubeVertexPositionBuffer.numItems = 24;
+
+    cubeVertexTextureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
+    var textureCoords = [
+        // Front face
+        1.0, 0.0,
+        0.75, 0.0,
+        0.75, 0.5,
+        1.0, 0.5,
+
+        // Back face
+        0.25, 0.0, 
+        0.25, 0.5,
+        0.5, 0.5, 
+        0.5, 0.0,
+
+        // Top face
+        0.25, 0.5, 
+        0.25, 1.0, 
+        0.5, 1.0, 
+        0.5, 0.5, 
+
+        // Bottom face
+        0.0, 0.5, 
+        0.25, 0.5, 
+        0.25, 1.0, 
+        0.0, 1.0, 
+
+        // Right face
+        0.5, 0.0, 
+        0.5, 0.5, 
+        0.75, 0.5, 
+        0.75, 0.0, 
+
+        // Left face
+        0.25, 0.0, 
+        0.0, 0.0, 
+        0.0, 0.5, 
+        0.25, 0.5
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+    cubeVertexTextureCoordBuffer.itemSize = 2;
+    cubeVertexTextureCoordBuffer.numItems = 24;
+
+    cubeVertexIndexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+    var cubeVertexIndices = [
+         0,  1,  2,      0,  2,  3, // Front face
+         4,  5,  6,      4,  6,  7, // Back face
+         8,  9, 10,      8, 10, 11, // Top face
+        12, 13, 14,     12, 14, 15, // Bottom face
+        16, 17, 18,     16, 18, 19, // Right face
+        20, 21, 22,     20, 22, 23  // Left face
+    ];
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
+    cubeVertexIndexBuffer.itemSize = 1;
+    cubeVertexIndexBuffer.numItems = 36;
 }
 
 function handleLoadedTexture(texture) {
@@ -285,8 +396,23 @@ function drawScene() {
     gl.uniform1i(shaderProgram.samplerUniform, 0);
 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, skyboxVertexIndexBuffer);
-    setMatrixUniformsSkybox();
+    setMatrixUniforms();
     gl.drawElements(gl.TRIANGLES, skyboxVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+
+    // render smaller cube
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cubeVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
+    gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, cubeVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, textureImage);
+    gl.uniform1i(shaderProgram.samplerUniform, 0);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+    setMatrixUniforms();
+    gl.drawElements(gl.TRIANGLES, cubeVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 }
 
 
